b8293bf63abcf6c95f285c7ce2a50c1428a61ec7
Tulagi <hujijun_lzu@163.com>
2022-02-27 22:54:57 +0800

    2022年02月27日 22:54:57: update gitbook

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,3 +1,4 @@
 # Summary
 
 * [介绍](README.md)
+* [编程语言](1_Languages/README.md)
--- docs/publish.sh
+++ docs/publish.sh
@@ -1,6 +1,7 @@
+set -e
+
 gitbook install
 gitbook build . docs
 git add .
 git commit -m "$(date): update gitbook"
 git push origin main:gitbook
-
