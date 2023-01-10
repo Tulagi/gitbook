899c7ef906d71d25a71927ff6d7ad78011ab6f63
Tulagi <hujijun_lzu@163.com>
2023-01-11 00:42:46 +0800

    2023年01月11日  0:42:46: main branch - update source

--- publish.sh
+++ publish.sh
@@ -4,9 +4,20 @@ set -x
 gitbook install
 gitbook build . docs
 
+mv docs/ ../gitbook_br_gitbook/
+
 git pull -p
 git add .
-git commit -m "$(date): publish gitbook"
+git commit -m "$(date): main branch - update source"
+git push origin main:main
+
+cd ../gitbook_br_gitbook/
+mv docs/* .
+rm -rf docs/
+rm -rf publish.sh
+git pull -p
+git add .
+git commit -m "$(date): gitbook branch - publish gitbook"
 git push origin gitbook:gitbook
 
 # git branch --set-upstream-to=origin/gitbook gitbook
