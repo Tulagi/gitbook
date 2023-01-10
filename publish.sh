901a779d76ae74f05af9c27fce5142b76a335f94
Tulagi <hujijun_lzu@163.com>
2023-01-11 01:12:55 +0800

    2023年01月11日  1:12:55: main branch - update source

--- publish.sh
+++ publish.sh
@@ -17,7 +17,7 @@ git push origin main:main
 # branch gitbook
 cd ../gitbook_br_gitbook/
 
-ls -a | grep -vP "docs/|\.git/|\./|\.\./" | xargs rm -rf
+ls | grep -vP "docs/" | xargs rm -rf
 mv docs/* .
 rm -rf docs/ publish.sh
 
